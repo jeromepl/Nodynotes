0918a52f81f1bdb1844cd79cfef5005aac5d09df
anecdot <jerome-pl2@hotmail.ca>
2014-06-03 14:11:27 -0400

    Added stripslashes and strip_tags to the user's name
    Added a TODO to salt passwords

--- server_side/isValidAccount.php
+++ server_side/isValidAccount.php
@@ -3,12 +3,12 @@
 	include("mySQL_connection.php"); //where $bdd is set	
 	session_start(); //start session
 	
-	if(isset($_POST['email']) && isset($_POST['password'])) { //NEED TO VERIFY EMAIL WITH REGEX
+	if(isset($_POST['email']) && isset($_POST['password'])) {
 		
 		$req = $bdd->prepare('SELECT id, name_first, name_last FROM users WHERE email = :email AND password = :password');
 		$req->execute(array(
 			'email' => $_POST['email'],
-			'password' => sha1($_POST['password'])));
+			'password' => sha1($_POST['password']))); //TODO Need to salt passwords
 		$answer = $req->fetch();
 		
 		$req->closeCursor();
@@ -18,8 +18,8 @@
 		}
 		else {
 			$_SESSION['id'] = $answer['id'];
-			$_SESSION['name_first'] = $answer['name_first'];
-			$_SESSION['name_last'] = $answer['name_last'];
+			$_SESSION['name_first'] = stripslashes(strip_tags($answer['name_first']));
+			$_SESSION['name_last'] = stripslashes(strip_tags($answer['name_last']));
 			header('Location: ../board.php');
 		}
 	}
