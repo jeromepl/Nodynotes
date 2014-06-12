63c5db88b3fec8dd9f38812b3c65007b92ed11cd
anecdot <jerome-pl2@hotmail.ca>
2014-06-11 20:48:14 -0400

    Completely re-created the home page, with a new banner. Javascript to come

--- home.php
+++ home.php
@@ -10,62 +10,104 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>Nody Notes - Learn and never forget again</title>
-        <!--[if lte IE 9]>
-        	<script>window.onload = function() {
-            	document.body.innerHTML= '<p>Please download the latest version of your browser</p>';
-            }</script>;
-        	<![endif]-->
         <link rel="SHORTCUT ICON" href="images/shortcut_icon.png">
         <link rel="stylesheet" type="text/css" href="styles/homeStyle.css">
         <script src="http://use.edgefonts.net/asap.js"></script>
 	</head>
-
-	<body>
-    	<header>
-        	<div id='head_container'>
-            	<div id="head_left">
-            		<span></span>
-        			<h1>Nody Notes</h1>
-                </div>
-                <div id="head_right">
-                	<span></span>
-                    <div id="connection">
-                    	<span></span>
-                        <form id="login_box" method="post" action="server_side/isValidAccount.php" >
-                            <input type="text" name="email" placeholder="Email" name="email" />
-                            <input type="password" name="password" placeholder="Password" name="password" />
-                            <input type="submit" id="login" class="button" value="Log in" />
-                        </form>
-                        <div id="connection_or">or</div>
-                        <a href="#" id="signup" class="button">Sign up</a>
-                    </div>
+    <body>
+        <header>
+            <div id='header'>
+                <div id='head_container' class='container'>
+                    <h1>NodyNotes</h1>
+                    <form method="post" action="server_side/isValidAccount.php" >
+                        <input type="text" name="email" placeholder="Email" />
+                        <input type="password" name="password" placeholder="Password" />
+                        <input type="submit" id="login" class="button" value="Log in" />
+                    </form>
                 </div>
             </div>
         </header>
-        <div id="banner">
-        	<img src="images/banner.png" id="banner_img">
-            <div id="description">
-            	<h2>Learn and never forget again</h2>
-                <p>Enjoy all the benefits of taking notes online. Remembering will become easy as pie.</p>
-                <a href="#">Check the video</a>
+
+        <div id='banner'>
+            <div id='banner_text' class='container'>
+                <div id='intro'>
+                    <h1>Learn and Never Forget Again</h1>
+                    <p>Save your knowledge and thoughts in an organized fashion. Never forget anything again</p>
+                </div>
+                <div id='signup_block'>
+                    <form method="post" action="#" >
+                        <div id="signup_name">
+                            <input type="text" name="name_first" placeholder="First Name" />
+                            <input type="text" name="name_last" placeholder="Last Name" />
+                        </div>
+                        <input type="text" name="email" placeholder="Email" autocomplete="off" />
+                        <input type="password" name="password" placeholder="Password" autocomplete="off" />
+                        <button type="submit" id="signup" class="button" >Sign up to NodyNotes!</button>
+                        <p>By clicking "Sign up to NodyNotes", you agree to our <a href="#">terms of service</a> and <a href="#">privacy policy</a>.</p>
+                    </form>
+                </div>
             </div>
         </div>
-        <div id="option_bar">
-        	<ul>
-            	<span></span>
-            	<li>Option 1</li>
-                <li>Option 2</li>
-                <li>Option 3</li>
-            </ul>
+
+        <div class='container'>
+            <h1 id='what'>What is NodyNotes?</h1>
+            <p id='what_description'>From taking chemistry notes at school to putting your family recipes online, NodyNotes got you covered. Type down anything you want to remember and acces it from anywhere in the world.<br>It's that easy!</p>
+            <div class='feature feature_left'>
+                <div class='feature_icon'><img data-src="images/icons/brush.svg" class="iconic iconic-lg"></div>
+                <div class='feature_text'>
+                    <h2>Create</h2>
+                    <p>Write your thoughts or knowledge down quickly and easily before forgetting them</p></p>
+                </div>
+            </div>
+            <div class='feature feature_right'>
+                <div class='feature_text'>
+                    <h2>Share</h2>
+                    <p>Expand your knowledge with your friends' notes and share yours</p>
+                </div>
+                <div class='feature_icon'><img data-src="images/icons/share-boxed.svg" class="iconic iconic-lg"></div>
+            </div>
+            <div class='feature feature_left'>
+                <div class='feature_icon'><img data-src="images/icons/magnifying-glass.svg" class="iconic iconic-lg"></div>
+                <div class='feature_text'>
+                    <h2>Search</h2>
+                    <p>Find, in a single click, everything you need in your notes with NodyNotes' powerful search algorithm</p>
+                </div>
+            </div>
+            <div class='feature feature_right'>
+                <div class='feature_text'>
+                    <h2>Organized</h2>
+                    <p>No more messy notes on pieces of paper. Your notes will always be clean and well organized in a beautiful and colored tree diagram</p>
+                </div>
+                <div class='feature_icon'><img data-src="images/icons/fork.svg" class="iconic iconic-lg"></div>
+            </div>
+            <div class='feature feature_left'>
+                <div class='feature_icon'><img data-src="images/icons/globe.svg" class="iconic iconic-lg"></div>
+                <div class='feature_text'>
+                    <h2>Accessible Everywhere</h2>
+                    <p>Access your notes anywhere in the world, at anytime, with all the benefits of cloud storage</p>
+                </div>
+            </div>
+            <div class='feature feature_right'>
+                <div class='feature_text'>
+                    <h2>Private</h2>
+                    <p>No one but you and the people you authorize can see your notes</p>
+                </div>
+                <div class='feature_icon'><img data-src="images/icons/shield.svg" class="iconic iconic-lg"></div>
+            </div>
         </div>
+
+        <div class='container'><a id='backToTop' href='#signup_block'class='button'>Try it now!</a></div>
         
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-        <script>
-			$(function() {
-				$(document).on('click', '#login', function(e) {
-					$('#login_box').show();
-				});
-			});
-		</script>
-	</body>
+        <div id='footer'>
+            <div class='container'>
+                <h3>©NodyNotes 2014</h3>
+                <div id='links'>
+                    <a href="blog/">Blog</a>
+                    <a href="#">Legal Stuff</a>
+                    <a href="#">Contact us</a>
+                </div>
+            </div>
+        </div>
+    </body>
+    <script src="plugins/iconic.min.js"></script>
 </html>
