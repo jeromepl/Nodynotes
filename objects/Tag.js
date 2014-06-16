6886c14e7a052910598bd6244cf1b347a584a91f
anecdot <jerome-pl2@hotmail.ca>
2014-06-15 23:04:26 -0400

    Added an id with id number for tags

--- objects/Tag.js
+++ objects/Tag.js
@@ -3,9 +3,9 @@ function Tag(id, title, node) {
 	this.title = title;
 	this.node = node;
 	
-	this.element = $('<div>').prependTo('#tags' + this.node.id).addClass('tag'); //set the id to the node's id to be able to select tags easily
+	this.element = $('<div>').prependTo('#tags' + this.node.id).addClass('tag').attr('id', 'tag' + this.id); //set the id to the node's id to be able to select tags easily
 	this.titleElement = $('<h5>').appendTo(this.element).text(this.title);
-	$('<img>').appendTo(this.element).attr({src: 'images/x.svg', id: 'tag_x_' + this.id}).addClass('iconic-sm tag_x');
+	$('<img>').appendTo(this.element).attr({src: 'images/icons/x.svg', id: 'tag_x_' + this.id}).addClass('iconic-sm tag_x');
 	var id = this.id;
 	iconic.inject('#tag_x_' + this.id, {}, function (count) {
 		$('#tag_x_' + id).show();
